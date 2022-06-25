130c725996c017e7b07ce9bfcb9333b29fb4e3da
ivanlyutov17 <lyutov17@yandex.ru>
2022-06-25 13:16:21 +0200

    hw-4

--- index.js
+++ index.js
@@ -1,39 +1,71 @@
 //1
 
-Array.prototype.myFilter = function (callback, thisArg){
-    var result = [];
-    let array = this;
-    let check;
-
-    if(callback ){
-
-        for(index = 0;index<array.length;index++){
-           let element = array[index];
-            check = callback.apply(thisArg,[element,index,array]);
-            if(check === true) result.push(element);
-        }     
-        return result
+function concatStrings(string = '', separator) {
+  var prevString = string;
+  const func = (nextStr = '') => {
+    func.valueOf = prevString;
+    if (typeof nextStr === 'string' && typeof prevString === 'string') {
+      prevString += `${separator}${nextStr}`;
+      func.valueOf = prevString;
+      return func;
+    } else {
+      const fakeFunc = (nextStr) => {
+        if (typeof prevString === 'string') {
+          fakeFunc.valueOf = prevString;
+          prevString = prevString;
+          return fakeFunc;
+        } else {
+          prevString = '';
+          return fakeFunc;
+        }
+      };
+      fakeFunc.valueOf = prevString;
+      return fakeFunc;
     }
-  
-else {
-    throw new Error("Не переданы условия фильтрации");
-}
+  };
+
+  return func;
 }
 
+// let a = concatStrings('vzasd',',')('asdfg')('avsa')('abcd')(123);
+// console.log(a.valueOf);
 
 //2
 
-function createDebounceFunction(fn, ms) {
-let timeout;
-    return function(){
-        const fnCall = ()=>{
-            fn.apply(this,arguments);
-        }
+class Calculator {
+  constructor(x, y) {
+    if (!isNaN(x) && !isNaN(y) && isFinite(x) && isFinite(y)) {
+      this.x = x;
+      this.y = y;
+    } else throw new Error('Передано невалидное значение!');
+  }
 
-        clearTimeout(timeout);
-
-        timeout = setTimeout(fnCall,ms);
-    }
-
+  setX = (x) => {
+    this.x = x;
   };
+  setY(y) {
+    this.y = y;
+  }
+  logSum = () => {
+    console.log(this.x + this.y);
+    return this.x + this.y;
+  };
+  logMul = () => {
+    console.log(this.x * this.y);
+    return this.x * this.y;
+  };
+  logSub = () => {
+    console.log(this.x - this.y);
+    return this.x - this.y;
+  };
+  logDiv = () => {
+    if (this.y === 0) {
+      throw new Error('На ноль нельзя делить');
+    } else {
+      console.log((this.x / this.y).toFixed(2));
+      return (this.x / this.y).toFixed(2);
+    }
+  };
+}
 
+let calc = new Calculator(5, 3);
