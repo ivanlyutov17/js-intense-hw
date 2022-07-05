f561dea7d3c327dbc78f3557e28641d0ea20d6ec
ivanlyutov17 <lyutov17@yandex.ru>
2022-07-05 11:23:03 +0200

    hw-5

--- stack.js
+++ stack.js
@@ -1,79 +1,126 @@
+///1
+
 class Stack {
-  constructor(length) {
-    if (length) this.maxLength = length;
-    else this.maxLength = 10;
-    this.currentLength = 0;
-    this.next = null;
+  constructor(capacity = 10) {
+    if (typeof capacity !== 'number' && typeof capacity !== 'null') {
+      throw new Error('Capacity of stack is not a number!');
+    }
+    this.capacity = capacity;
+    this.elements = [];
+    this.length = 0;
+  }
+
+  push(elem) {
+    if (this.length === this.capacity) {
+      throw new Error('Stack overflow!');
+    }
+    this.elements[this.length] = elem;
+    this.length++;
+  }
+  pop() {
+    if (!this.length) {
+      throw new Error('Stack is empty!!!');
+    }
+    this.length--;
+    this.deleted = this.elements[this.length];
+    delete this.elements[this.length];
+    return this.deleted;
+  }
+  peek() {
+    if (!this.length) {
+      return null;
+    }
+    return this.elements[this.length - 1];
+  }
+
+  isEmpty() {
+    return !this.length;
+  }
+
+  toArray() {
+    let result = [];
+    let copy = Object.assign(new Stack(), structuredClone(this));
+    while (copy.peek()) {
+      result.push(copy.pop());
+    }
+    result = result.reverse();
+    return result;
+  }
+
+  static fromIterable(iterable) {
+    if (typeof iterable[Symbol.iterator] !== 'function') {
+      throw new Error(`Not iterable!`);
+    }
+    let newStack = new Stack();
+    for (let e of iterable) {
+      newStack.push(e);
+    }
+    return newStack;
+  }
+}
+
+///2
+
+class LinkedList {
+  constructor() {
     this.head = null;
     this.tail = null;
   }
 
-  push(value) {
-    this.currentLength++;
+  append(value) {
+    const newNode = Object.create(null);
+    newNode.value = value;
+    newNode.next = null;
 
-    if (this.currentLength <= this.maxLength) {
-      const newNode = Object.create(null);
-      newNode.value = value;
-      newNode.next = this.head;
+    if (!this.head || !this.tail) {
       this.head = newNode;
-      if (!this.tail) {
-        this.tail = newNode;
-      }
+      this.tail = newNode;
 
       return this;
-    } else throw new Error('Стэк переполнен');
+    }
+
+    this.tail.next = newNode;
+
+    this.tail = newNode;
+
+    return this;
   }
 
-  pop() {
+  prepend(value) {
+    const newNode = Object.create(null);
+    newNode.value = value;
+    newNode.next = this.head;
+
+    this.head = newNode;
+
     if (!this.tail) {
-      this.currentLength = 0;
-      throw new Error('Стэк пуст');
+      this.tail = newNode;
     }
 
-    this.currentLength--;
-    const deletedTail = this.tail;
-
-    if (this.head === this.tail) {
-      this.head = null;
-      this.tail = null;
-
-      return deletedTail;
-    }
-
-    let currentNode = this.head;
-    while (currentNode.next) {
-      if (!currentNode.next.next) {
-        currentNode.next = null;
-      } else {
-        currentNode = currentNode.next;
-      }
-    }
-
-    this.tail = currentNode;
-
-    return deletedTail;
+    return this;
   }
 
-  peek() {
-    if (!this.tail) {
+  find(value) {
+    if (!this.head) {
       return null;
     }
+
     let currentNode = this.head;
-    if (this.currentLength === 1) return this.head;
-    if (this.isEmpty()) throw new Error('Стэк пустой');
-    while (currentNode.next !== null) {
+
+    while (currentNode) {
+      if (value !== undefined && currentNode.value === value) {
+        return currentNode;
+      }
+
       currentNode = currentNode.next;
     }
-    return currentNode;
-  }
 
-  isEmpty() {
-    if (this.currentLength === 0) return true;
-    else return false;
+    return null;
   }
 
   toArray() {
     const nodes = [];
+
     let currentNode = this.head;
 
     while (currentNode) {
@@ -86,20 +133,14 @@ class Stack {
 
   static fromIterable(iterable) {
     let nodeFromIterable;
-    let maxLength = 0;
     if (typeof iterable[Symbol.iterator] === 'function') {
-      nodeFromIterable = new Stack();
+      nodeFromIterable = new LinkedList();
       for (const elem of iterable) {
-        maxLength++;
-        nodeFromIterable.push(elem);
+        nodeFromIterable.append(elem);
       }
-      nodeFromIterable.maxLength = maxLength;
       return nodeFromIterable;
     } else throw new Error('Non-iterable');
   }
 }
-
-let test = new Stack();
-let fromIt = Stack.fromIterable(['1', '2', '3']);
 
 module.exports = { Stack };
